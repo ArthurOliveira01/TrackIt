d52fceae6b5d75b0549cc79b2f4991e51ba3ecf0
Arthur Oliveira da Silva <e.arthur.osilva@mpt.mp.br>
2023-03-20 13:08:34 -0400

    list habits

--- src/pages/HabitsPage/HabitsPage.jsx
+++ src/pages/HabitsPage/HabitsPage.jsx
@@ -1,8 +1,10 @@
-import { useEffect, useContext } from "react";
+import { useEffect, useContext, useState } from "react";
 import styled from "styled-components";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import Context from "../../components/Context";
+import Habitos from "../../components/Habitos"
+import axios from "axios";
 
 
 const Row = styled.div`
@@ -57,30 +59,61 @@ const None = styled.p`
 
 const Container = styled.div`
     width: 100%;
-    height: 1000px;
+    height: 100%;
     display: flex;
     flex-direction: column;
     background-color: #E5E5E5;
     padding-bottom: 70px;
 `;
 
+const Creating = styled.div`
+    width: 90.66666667%;
+    height: 180px;
+    background-color: #FFFFFF;
+    margin-top: 10px;
+    border-radius: 5px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: left;
+    background-color: #FFFFFF;
+    margin-top: 10px;
+    margin-bottom: 29px;
+`;
 
 export default function HabitsPage(){
     const {token} = useContext(Context);
+    const [novo, setNovo] = useState(false);
+    const [lista, setLista] = useState([]);
 
     useEffect(() =>{
+        const listar = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits", {
+            headers: {Authorization: `Bearer ${token}`}
+        });
 
+        listar.then(promise => {
+            setLista(promise.data)
+        })
     })
 
+    function test(){
+        console.log(lista);
+    }
+        
+//<None>Você não tem nenhum hábito cadastrado ainda. Adicione um hábito para começar a trackear!</None>
     return(
         <Container>
             <Header/>
             <Row>
                 <Mine>Meus hábitos</Mine>
-                <Create data-test="habit-create-btn">+</Create>
+                <Create onClick={() => setNovo(true)} data-test="habit-create-btn">+</Create>
             </Row>
             <HabitsContainer>
+                {novo && (<Creating></Creating>)}
+                {lista.length === 0 && (
                 <None>Você não tem nenhum hábito cadastrado ainda. Adicione um hábito para começar a trackear!</None>
+                )}
+                {lista.map(habit => <Habitos unidade={habit}></Habitos>)}
             </HabitsContainer>
             <Footer />
         </Container>
